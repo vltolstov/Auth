9be5198e5016b3dd5215473a6a25704d198c890e
Vladimir <tolstov@gmail.com>
2024-04-02 11:00:47 +0700

    Базовые настройки

---
+++ v10_auth_prototype/resources/views/index.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Тест</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500;700&family=Roboto&display=swap" rel="stylesheet">
+        <!-- Styles -->
+        @vite('resources/css/app.css')
+    </head>
+    <body>
+        <div class="container">
+            <p>Тест</p>
+        </div>
+
+        @vite('resources/js/app.js')
+    </body>
+</html>

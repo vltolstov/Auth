69b3f7c508893f3d549ef11947d7d303ad594141
Vladimir <tolstov@gmail.com>
2024-05-23 14:18:21 +0700

    Исправлена ошибка записи id роли

--- v10_auth_prototype/app/Models/User.php
+++ v10_auth_prototype/app/Models/User.php
@@ -21,7 +21,7 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
-        'role',
+        'role_id',
         'phone',
         'email',
         'password',
